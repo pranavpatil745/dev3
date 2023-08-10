016c316bf34f5873371959fa2dee260459d33a27
pranavpatil745 <pranavpatil745@gmail.com>
2023-08-10 11:50:33 +0530

    third commit

--- ty.py
+++ ty.py
@@ -1,1 +1,6 @@
 echo "This is trial file is this reallly git file "
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print("{0} is Even".format(num))
+else:
+   print("{0} is Odd".format(num))
